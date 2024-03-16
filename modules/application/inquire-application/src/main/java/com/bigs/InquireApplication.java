2c28047ca860697c772e9e21a5ba3a1ba12b7754
sHu <akgkfk9@gmail.com>
2024-03-16 22:25:05 +0900

    개발 환경 세팅 (Multi-module)

---
+++ modules/application/inquire-application/src/main/java/com/bigs/InquireApplication.java
@@ -0,0 +1,12 @@
+package com.bigs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InquireApplication.class, args);
+    }
+}
