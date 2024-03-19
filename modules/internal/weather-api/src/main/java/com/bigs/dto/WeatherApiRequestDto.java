2f03fde186ac1d96a72fcbe7d47037a4459a4a62
sHu <akgkfk9@gmail.com>
2024-03-20 01:00:05 +0900

    add: (내부 모듈) Bean Validation 라이브러리 추가 및 WeatherRequestDto에 Validation 적용

--- modules/internal/weather-api/src/main/java/com/bigs/dto/WeatherApiRequestDto.java
+++ modules/internal/weather-api/src/main/java/com/bigs/dto/WeatherApiRequestDto.java
@@ -1,21 +1,37 @@
 package com.bigs.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Range;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter @Builder
 @ToString
 public class WeatherApiRequestDto {
+
+    @NotNull
+    @Range(min = 1, max = 100)
     private Integer pageNo;                 // 페이지 번호
 
+    @NotNull
+    @Range(min = 1, max = 1000)
     private Integer numOfRows;              // 한 페이지 결과 수
 
+    @NotNull
+    @DateTimeFormat(pattern = "yyyyMMdd")
     private String baseDate;                // 발표 일자
 
+    @NotNull
+    @DateTimeFormat(pattern = "HHmm")
     private String baseTime;                // 발표 시각
 
+    @NotNull
+    @Range(min = 1, max = 200)
     private Integer nx;                     // X 좌표
 
+    @NotNull
+    @Range(min = 1, max = 200)
     private Integer ny;                     // Y 좌표
 }
